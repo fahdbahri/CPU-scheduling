e998b44f460ea4314c7042d298bc1e62f27f26c0
fahdbahri <fahdkhalid2002@gmail.com>
2024-03-02 13:12:59 +0200

    fcfs is completed

--- fcfs.c
+++ fcfs.c
@@ -62,13 +62,45 @@ int serialize(List* L, const char* file)
 }
 
 
-double fcfs_method(List* L)
+void printfcfs_method(List* L, FILE *output_file)
 {  
-    printf("Scheduling Method: First Come First Served\n");
+    fprintf(output_file, "Scheduling Method: First Come First Served\n");
+    fprintf(output_file, "Process Waiting Times:\n");
+
+    if (L == NULL && L->size == 0 ) return;
+    double avgWait_Time = 0.;
+    int totalWait_time = 0;
+    int current_time = 0;
+    Node*  p   = L->head;
+    for (size_t i = 0; i < L->size; i += 1)
+    {  // here we have node data, one
+        // at a time
+        p->info->start_time = current_time;
+        p->info->complete_time = p->info->start_time + p->info->burst;
+        p->info->turn_around = p->info->complete_time - p->info->arrival;
+        p->info->wait_time = p->info->turn_around - p->info->burst;
+
+
+        totalWait_time += p->info->wait_time;
+        current_time = p->info->complete_time;
+       fprintf(output_file, "P%ld: %d ms\n", i + 1, p->info->wait_time);
+        p = p->next;
+    }
+
+   
+
+    avgWait_Time = (double)totalWait_time / size(L);
+
+   fprintf(output_file, "Average waiting time is: %.2f\n", avgWait_Time); 
+
+}
+
+void fcfs_method(List* L)
+{  
+    printf( "Scheduling Method: First Come First Served\n");
     printf("Process Waiting Times:\n");
 
-    if (L == NULL) return -1;
-    if (L->size == 0) return 0.;  // easy
+    if (L == NULL && L->size == 0 ) return;
     double avgWait_Time = 0.;
     int totalWait_time = 0;
     int current_time = 0;
@@ -91,6 +123,7 @@ double fcfs_method(List* L)
    
 
     avgWait_Time = (double)totalWait_time / size(L);
-    return avgWait_Time;
+
+   printf("Average waiting time is: %.2f\n", avgWait_Time); 
 
 }
